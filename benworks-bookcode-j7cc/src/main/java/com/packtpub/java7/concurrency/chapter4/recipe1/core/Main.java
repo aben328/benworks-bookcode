6c70fd7baaeebe40407feade9b7675848c9229dd
aben328 <aben328@gmail.com>
2014-03-26 22:39:57 +0800

    1

---
+++ benworks-bookcode-j7cc/src/main/java/com/packtpub/java7/concurrency/chapter4/recipe1/core/Main.java
@@ -0,0 +1,29 @@
+package com.packtpub.java7.concurrency.chapter4.recipe1.core;
+
+import com.packtpub.java7.concurrency.chapter4.recipe1.task.Server;
+import com.packtpub.java7.concurrency.chapter4.recipe1.task.Task;
+
+/**
+ * Main class of the example. Creates a server and 100 request of the Task class that sends to the server
+ */
+public class Main {
+
+	/**
+	 * Main method of the example
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Create the server
+		Server server = new Server();
+
+		// Send 100 request to the server and finish
+		for (int i = 0; i < 100; i++) {
+			Task task = new Task("Task " + i);
+			server.executeTask(task);// 不需要为每一个Runnable接口创建一个Thread对象
+		}
+
+		server.endServer();
+
+	}
+
+}
